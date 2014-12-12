f8e7f7d5e71fa7fe75eca6e286e840c86cec2d06
clowfreeman <claudiupardos@gmail.com>
2014-12-12 03:30:41 +0200

    Create TCPServer class and TCPClient class

---
+++ src/com/se/project/tcp/connection/layer/TCPClient.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.se.project.tcp.connection.layer;
+
+/**
+ * @author ClPardos
+ *
+ */
+public class TCPClient implements TCPConnectionPoint {
+
+}
---
+++ src/com/se/project/tcp/connection/layer/TCPServer.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package com.se.project.tcp.connection.layer;
+
+/**
+ * @author ClPardos
+ *
+ */
+public class TCPServer implements TCPConnectionPoint {
+
+}
