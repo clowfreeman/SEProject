e0ed7f45ba82ffa7a176a6ca46dc4fdd6905bf93
clowfreeman <claudiupardos@gmail.com>
2014-12-17 16:09:27 +0200

    Create Installer class

---
+++ src/com/se/project/installation/Installer.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package com.se.project.installation;
+
+/**
+ * @author ClPardos
+ *
+ */
+public class Installer {
+	public String username;
+	public String password;
+	public String getUsername() {
+        return username;
+
+    }
+    public void setUsername(String username) {
+        this.username = username;
+
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public void checkInstaller(){
+    	Valid a =new Valid();
+    	a.validate(username,password);
+    }
+}
+
+class Valid{
+	Installer i;
+	CertificateGenerator cg;
+	public void validate(String username,String password){
+		if ((i.username).equals(cg.username)&&(i.password).equals(cg.password)){
+			// Aici se face instalarea
+		}
+		else {
+			System.out.println("Username sau Password incorecte!");
+		}
+	}
+}
