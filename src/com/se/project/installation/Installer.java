e4a829f5caab3b3bfac2bc1e2c917d8497c29009
clowfreeman <claudiupardos@gmail.com>
2014-12-17 23:44:42 +0200

    Change Install class

--- src/com/se/project/installation/Installer.java
+++ src/com/se/project/installation/Installer.java
@@ -8,29 +8,32 @@ package com.se.project.installation;
  *
  */
 public class Installer {
-	public String username;
-	public String password;
-	public String getUsername() {
-        return username;
+	public static void install(String username,String password){
+		CertificateGenerator.generate(username,password);
+	}
+	//public String username;
+	//public String password;
+	//public String getUsername() {
+    //    return username;
 
-    }
-    public void setUsername(String username) {
-        this.username = username;
+    //}
+    //public void setUsername(String username) {
+    //    this.username = username;
 
-    }
-    public String getPassword() {
-        return password;
-    }
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    public void checkInstaller(){
-    	Valid a =new Valid();
-    	a.validate(username,password);
-    }
+    //}
+    //public String getPassword() {
+    //    return password;
+    //}
+    //public void setPassword(String password) {
+    //    this.password = password;
+    //}
+    //public void checkInstaller(){
+    //	Valid a =new Valid();
+    //	a.validate(username,password);
+    //}
 }
 
-class Valid{
+/*class Valid{
 	Installer i;
 	CertificateGenerator cg;
 	public void validate(String username,String password){
@@ -41,4 +44,4 @@ class Valid{
 			System.out.println("Username sau Password incorecte!");
 		}
 	}
-}
+}*/
